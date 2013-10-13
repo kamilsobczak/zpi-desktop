35f48cd263a7951b38b7cdccfd64fd9c2a3c9e32
adasq <adasq@migmail.com>
2013-10-13 12:10:14 +0200

    test

---
+++ src/com/zpi/desktop/init/main.java
@@ -0,0 +1,13 @@
+package com.zpi.desktop.init;
+
+public class main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello 2!");
+	}
+
+}
